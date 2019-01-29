3736429e0d8594ce450a1192ac28a3b8eec2b066
eladtamari <44618706+eladtamari@users.noreply.github.com>
2019-01-29 16:50:11 +0200

    print res

--- editJson.py
+++ editJson.py
@@ -5,7 +5,11 @@ with open("Config.json", 'r+') as f:
     data = json.load(f)
     data['install']['version'] = "1.0.0.{0}".format(sys.argv[1]) # <--- add `id` value.
     f.seek(0)        # <--- should reset file position to the beginning.
+    
+    print data['install']['version']
     json.dump(data, f, indent=4)
+    
+   
 
 
 
