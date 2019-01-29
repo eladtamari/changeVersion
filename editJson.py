181520dc87262f024e5e2f4a16994c328cfdd6f7
eladtamari <44618706+eladtamari@users.noreply.github.com>
2019-01-29 20:19:43 +0200

    change json to JSON in python

--- editJson.py
+++ editJson.py
@@ -1,7 +1,7 @@
 import json
 import sys
 
-with open("Config.json", 'r+') as f:
+with open("Config.JSON", 'r+') as f:
     data = json.load(f)
     data['install']['version'] = "1.0.0.{0}".format(sys.argv[1]) # <--- add `id` value.
     f.seek(0)        # <--- should reset file position to the beginning.
