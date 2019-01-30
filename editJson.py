8ba353e93ddd777059ac455cca17461626395e70
eladtamari <44618706+eladtamari@users.noreply.github.com>
2019-01-30 12:06:31 +0200

    Update editJson.py

--- editJson.py
+++ editJson.py
@@ -1,14 +1,15 @@
 import json
 import sys
 
-with open("Config.JSON", 'r+') as f:
+with open("Config.json", 'r+') as f:
     data = json.load(f)
     data['install']['version'] = "1.0.0.{0}".format(sys.argv[1]) # <--- add `id` value.
     f.seek(0)        # <--- should reset file position to the beginning.
     print data['install']['version']
     json.dump(data, f, indent=4)
-
-
+    with open("resConfig.json", "r+") as g:
+        json.dump(data, g, indent=4)
+        
 
 
 
